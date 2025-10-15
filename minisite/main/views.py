ecedd32fbb12f965b1b85dad24d6289668ffec13
Tadiwanashe Makoni <tadiwa.makoni13@gmail.com>
2025-10-15 12:53:11 +0200

    updated pics

--- minisite/main/views.py
+++ minisite/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.core.paginator import Paginator
 from .models import Product
 import random
+
 
 # 🏠 Home Page (random featured product)
 def home(request):
@@ -13,10 +16,15 @@ def home(request):
     })
 
 
-# 🛍 Product List Page
+# 🛍 Product List Page with Pagination
 def products(request):
-    products = Product.objects.all()
-    return render(request, 'products.html', {'products': products})
+    product_list = Product.objects.all().order_by('id')  # Show all products ordered by ID
+    paginator = Paginator(product_list, 8)  # 8 products per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'products.html', {'products': page_obj})
 
 
 # 📦 Product Detail Page
@@ -29,18 +37,16 @@ def product_details(request, product_id):
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
 
-    # Get or create cart from session
     cart = request.session.get('cart', {})
-
-    # Add or increment product quantity
     if str(product_id) in cart:
         cart[str(product_id)] += 1
+        messages.info(request, f"Added another {product.name} to your cart.")
     else:
         cart[str(product_id)] = 1
+        messages.success(request, f"{product.name} added to your cart!")
 
     request.session['cart'] = cart
     request.session.modified = True
-
     return redirect('cart')
 
 
@@ -69,8 +75,12 @@ def cart(request):
 # ❌ Remove item from cart
 def remove_from_cart(request, product_id):
     cart = request.session.get('cart', {})
+    product = get_object_or_404(Product, id=product_id)
+
     if str(product_id) in cart:
         del cart[str(product_id)]
         request.session['cart'] = cart
         request.session.modified = True
+        messages.error(request, f"{product.name} removed from your cart.")
+
     return redirect('cart')
